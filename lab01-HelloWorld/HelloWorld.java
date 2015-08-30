8f009ea8fc558712978150e3d7eb8b8937d45f5a
yil219 <yil219@lehigh.edu>
2015-08-30 19:17:25 +0000

    updated lab1

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////////////////////
+//Yiming Lu
+//Hello World Java Program
+//
+//  first compile the program 
+//      javac HelloWorld.java
+//  run the program
+//      java HelloWorld//
+
+
+//define a class
+public class HelloWorld {
+    
+//add main method
+public static void main(String[] args) {
+        
+        //print the statement with the box around it
+        System.out.println("##################################################");
+        System.out.println("##################################################");
+        System.out.println("#                                                #");
+        System.out.println("#           Hello World 2014!!                   #");
+        System.out.println("#                                                #");
+        System.out.println("##################################################");
+        System.out.println("##################################################");
+        System.out.println("test");
+        //test//testtest
+    }
+}   
