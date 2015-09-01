3411c943758bfb0be6a538890314fa36ff83edc1
yil219 <yil219@lehigh.edu>
2015-09-01 20:47:01 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,34 @@
+////////////////////////////////////////////////////////////////////////////////
+//Yiming Lu
+//Welcome Class Java Program
+//
+//  first compile the program 
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//define a class
+
+public class WelcomeClass {
+
+//add main method
+public static void main(String[] args) {
+
+//print the statement with the box around it
+
+ System.out.println("-----------");
+ System.out.println("| WELCOME |");
+ System.out.println("-----------");
+ System.out.println("^ ^ ^ ^ ^ ^");
+ System.out.println("/\\ /\\ /\\ /\\ /\\ /\\");
+ System.out.println("|-Y--I--L--2--1--9-|");
+ System.out.println("\\/ \\/ \\/ \\/ \\/ \\/");
+ System.out.println("v v v v v v");
+ System.out.println("This August my wife and I took an eleven-day, 675-mile bike tour on our tandem. We circled Lake Ontario.");
+}
+}
+
+ 
+ 
+ 
+
